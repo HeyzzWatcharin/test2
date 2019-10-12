afdf9e7e8a864d4bdd4463f440771f3d48feb1c1
job25721 <amashiikaze@gmail.com>
2019-10-12 13:51:04 +0700

    first

---
+++ src/pages/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <img src="../../public/img/index.jpg" alt="" class="index mt-5">
+    <br>
+    
+    <div class="center">
+      <router-link :to="{path:'/login'}"><button class="btn button" > Enter Site </button> </router-link>
+    </div>
+    <br>
+    <div class="center ">
+      <img class="logo mr-3 ml-3" src="../../public/img/Gear.png" alt="">
+      <img class="logo mr-3 ml-3" src="../../public/img/cpe.png" alt="">
+    </div>
+    <br>
+    <p class="center index-text">
+      - Department of Computer Engineering, Chiang Mai University -
+    </p>
+    
+    
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+  .logo{
+    max-width: 5%;
+  }
+  .index{
+    position: relative;
+    display: block;
+    max-width: 80%;
+    
+    border-radius: 5px;
+    box-shadow: 0px 2px 7px #505050;
+    
+    margin-left: auto;
+    margin-right: auto;
+    
+  }
+  .center{
+    text-align :center;
+  }
+  .button{
+    border-radius: 25px;
+    background-color:rgb(27, 27, 27);
+    color: white;
+  }
+  .index-text{
+    color:rgb(51, 51, 51);
+    font-size: 18px;
+  }
+</style>
---
+++ src/pages/login.vue
@@ -0,0 +1,91 @@
+<template>
+  <!-- Default form login -->
+  <div class="main">
+    <form v-on:submit="login">
+      <p class="h4 text-center mb-4">Sign in</p>
+      <label for="defaultFormLoginEmailEx" class="grey-text">Your email</label>
+      <input v-model="username" type="text" name="email" id="defaultFormLoginEmailEx" class="form-control"/>
+      <br/>
+      <label for="defaultFormLoginPasswordEx" class="grey-text">Your password</label>
+      <input v-model="password" type="password" name="password" id="defaultFormLoginPasswordEx" class="form-control"/>
+        <div class="text-center mt-4">
+          
+            <button class="btn btn-indigo" type="submit" value="Login">Login</button>
+          
+        </div>
+    </form>
+  </div>
+  <!-- Default form login -->
+</template>
+<script>
+  import { mdbInput, mdbBtn } from 'mdbvue';
+  import axios from "axios"
+  import router from '../routes/routes'
+import Axios from 'axios';
+  export default {
+    name: 'Login',
+    data :{
+      username : '',
+      password : ''
+    },
+    methods:{
+      login: (e) => {    
+        // e.preventDefault()   
+        // let email = e.target.elements.email.value
+        // let password = e.target.elements.password.value
+        let login = () => {
+          let data = {
+            username: username,
+            password: password
+          }
+          axios.post("api/login", data)
+          .then((response) => {
+            console.log("Logged in")
+            router.push("/admin")
+          })
+          .catch((errors) => {
+            console.log("Cannot login")
+          })
+        }
+      login()      
+    },
+    components: {
+      mdbInput,
+      mdbBtn
+    },
+    }
+  }
+</script>
+
+<style scoped>
+  .main{
+      margin-left: auto;
+      margin-right: auto;
+      margin-top: auto;
+      max-width: 30%;
+      position: relative;
+      
+  }
+  .form-simple .font-small {
+    font-size: 0.8rem; }
+
+  .form-simple .header {
+    border-top-left-radius: .3rem;
+    border-top-right-radius: .3rem; }
+
+  .form-simple input[type=text]:focus:not([readonly]) {
+    border-bottom: 1px solid #ff3547;
+    -webkit-box-shadow: 0 1px 0 0 #ff3547;
+    box-shadow: 0 1px 0 0 #ff3547; }
+
+  .form-simple input[type=text]:focus:not([readonly]) + label {
+    color: #4f4f4f; }
+
+  .form-simple input[type=password]:focus:not([readonly]) {
+    border-bottom: 1px solid #ff3547;
+    -webkit-box-shadow: 0 1px 0 0 #ff3547;
+    box-shadow: 0 1px 0 0 #ff3547; }
+
+  .form-simple input[type=password]:focus:not([readonly]) + label {
+    color: #4f4f4f; }
+</style>
